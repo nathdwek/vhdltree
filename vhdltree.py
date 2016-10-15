7ea45365a146951d94e619c60e2a116f5f0979f5
nath@laptop <nathdwek@ulb.ac.be>
2016-10-15 18:07:04 +0200

    quotes

--- vhdltree.py
+++ vhdltree.py
@@ -5,8 +5,9 @@ import os
 import os.path
 from sys import argv
 
-EXCLUDES = ["implementation", "testbench"]
-BASIC_ID_REGEX = "[a-z][a-z0-9]*(?:_[a-z0-9]+)*"
+
+EXCLUDES = ['implementation', 'testbench']
+BASIC_ID_REGEX = '[a-z][a-z0-9]*(?:_[a-z0-9]+)*'
 
 
 def _vhdltree(level, filepath, pattern, vhd_files):
@@ -25,13 +26,13 @@ def find_entities(lines, pattern):
     for l in lines:
         m = pattern.match(l)
         if m:
-            yield m.group('entity'), m.group('component').split(".")[-1]
+            yield m.group('entity'), m.group('component').split('.')[-1]
 
 
 def find_vhd(directory):
     for entry in os.listdir(directory):
         entrypath = os.path.join(directory, entry)
-        if os.path.isfile(entrypath) and entry[-4:].lower() == ".vhd":
+        if os.path.isfile(entrypath) and entry[-4:].lower() == '.vhd':
                 yield entry[:-4].lower(), entrypath
         elif os.path.isdir(entrypath):
             if all(excluder not in entry.lower() for excluder in EXCLUDES):
@@ -40,12 +41,12 @@ def find_vhd(directory):
 
 
 def vhdltree(filepath, proot):
-    instantiation_regex = ("\s*(?P<entity>{0})\s*:\s*entity\s*(?P<component>{0}(?:\.{0})*)"  # NOQA
+    instantiation_regex = ('\s*(?P<entity>{0})\s*:\s*entity\s*(?P<component>{0}(?:\.{0})*)'  # NOQA
                            .format(BASIC_ID_REGEX))
     p = re.compile(instantiation_regex, re.IGNORECASE)
     vhd_files = dict(find_vhd(proot))
     for level, entity, path in _vhdltree(0, filepath, p, vhd_files):
         print("    "*level + entity + " : " + path)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     vhdltree(argv[1], argv[2])
