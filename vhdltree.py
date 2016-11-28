2e647e9f04085225b9ef7639baa975c5b1d9691d
nath@thesis <nathdwek@ulb.ac.be>
2016-11-28 10:20:21 +0100

    Tweak and fix:
    Use global compiled pattern
    Use context manager to access files (were not closed before I think)

--- vhdltree.py
+++ vhdltree.py
@@ -13,22 +13,34 @@ BASIC_ID_REGEX = (
     r'[a-z0-9]*'        # it can contain alphanumerics
     r'(?:_[a-z0-9]+)*'  # and '_' but not at the end
 )
+INSTANTIATION_REGEX = (
+    r'\s*'               # indentation
+    r'(?P<entity>{0})'   # single basic identifier
+    r'\s*:\s*entity\s*'  # entity declaration and spacing
+    r'(?P<component>'    # component:
+        r'{0}'            # at least a basic identifier # noqa E131
+        r'(?:\.{0})*'     # for libraries: dots can only appear if they are followed by another basic identifier
+    r')'
+    .format(BASIC_ID_REGEX)
+)
+INSTANTIATION_PATTERN = re.compile(INSTANTIATION_REGEX, re.IGNORECASE)
 
 
-def _vhdltree(level, filepath, pattern, vhd_files):
-    for entity, component in find_entities(open(filepath), pattern):
-        try:
-            path = vhd_files[component.lower()]
-        except KeyError:
-            yield level, entity, ''
-        else:
-            yield level, entity, path
-            yield from _vhdltree(level + 1, path, pattern, vhd_files)
+def _vhdltree(level, vhd_path, vhd_files):
+    with open(vhd_path) as vhd_file:
+        for entity, component in find_entities(vhd_file):
+            try:
+                path = vhd_files[component.lower()]
+            except KeyError:
+                yield level, entity, ''
+            else:
+                yield level, entity, path
+                yield from _vhdltree(level + 1, path, vhd_files)
 
 
-def find_entities(lines, pattern):
+def find_entities(lines):
     for l in lines:
-        m = pattern.match(l)
+        m = INSTANTIATION_PATTERN.match(l)
         if m:
             yield m.group('entity'), m.group('component').split('.')[-1]
 
@@ -44,19 +56,8 @@ def find_vhd(directory):
 
 
 def vhdltree(filepath, proot):
-    instantiation_regex = (
-        r'\s*'               # indentation
-        r'(?P<entity>{0})'   # single basic identifier
-        r'\s*:\s*entity\s*'  # entity declaration and spacing
-        r'(?P<component>'    # component:
-            r'{0}'            # at least a basic identifier # noqa E131
-            r'(?:\.{0})*'     # for libraries: dots can only appear if they are followed by another basic identifier
-        r')'
-        .format(BASIC_ID_REGEX)
-    )
-    p = re.compile(instantiation_regex, re.IGNORECASE)
     vhd_files = dict(find_vhd(proot))
-    for level, entity, path in _vhdltree(0, filepath, p, vhd_files):
+    for level, entity, path in _vhdltree(0, filepath, vhd_files):
         print('{indent}{entity} : {path}'.format(indent=4 * ' ' * level,
                                                  entity=entity,
                                                  path=path or 'Not found'))
