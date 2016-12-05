b5f0c44384cac4f51d029e76fb33a710af462f56
nath@home <nathdwek@ulb.ac.be>
2016-12-05 22:58:37 +0100

    `component` is a used vhdl keyword so don't misuse it in tests

--- test/dummy_project/main.vhd
+++ test/dummy_project/main.vhd
@@ -2,10 +2,10 @@ e1i1: entity long_component_name5678
 	e1i2 : entity long_component_name5678
  e2I1:entity E2 --comment
 E3_i1:	entity lib.e3
-_badid: entity component
-badid_: entity component
+_badid: entity comp
+badid_: entity comp
 goodid1: entity .badcomponent
 truncate_before_dot: entity e4.
-bad_id_é"'(-è_ç)'": entity component
+bad_id_é"'(-è_ç)'": entity comp
 the: entity E_1
 not_found: entity not_in_directory
